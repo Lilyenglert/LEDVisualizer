6e94ea59dbb45ff88f5645020ee5bd10a8651943
lmc327 <lmc327@cornell.edu>
2019-12-08 17:57:25 -0500

    sound/light/motion separation

---
+++ light/light_functionality.ino
@@ -0,0 +1,13 @@
+// things particular to light functionality can go here
+// try and anticipate where integrations with sound & motion
+// code will happen and comment accordingly
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
